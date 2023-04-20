9ad03d443db6249464b566e118de6b7dfe5f35d6
Pontsho04 <mabopontsho>
2023-04-20 09:58:39 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * Description: main - Prints out a string to stdout.
+ */
+int main<void>
+{
+printf("with proper grammer, but the outcome is a piece of art,\n");
+return (0);
+}
