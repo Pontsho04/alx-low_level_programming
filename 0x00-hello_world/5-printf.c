e391f1cba45d5991d217193a15935e434908847f
Pontsho04 <mabopontsho>
2023-04-20 15:24:56 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * Return 0 if success.
  * Description: main - Prints out a string to stdout.
+ * Return: 0 if success.
  */
-int main<void>
+int mai(void)
 {
-printf("with proper grammer, but the outcome is a piece of art,\n");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
