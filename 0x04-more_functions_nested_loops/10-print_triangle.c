a5ef450795de110d787491616c38f16a4cc6e3bb
Pontsho04 <mabopontsho>
2023-04-28 15:47:13 -0700

    10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -19,13 +19,13 @@ void print_triangle(int size)
 	{
 		for (row = 1; row <= size; row++)
 		{
-			for (spaces = size; row; spaces >= 1; spaces--)
+			for (spaces = size - row; spaces >= 1; spaces--)
 			{
 				_putchar(' ');
 			}
 			for (hashes = 1; hashes <= row; hashes++)
 			{
-				-putchar('#');
+				_putchar('#');
 			}
 			_putchar('\n');
 		}
