2a7ceef3f7998a6fa45626a7087ff09be39a1288
Pontsho04 <mabopontsho>
2023-04-28 08:44:55 -0700

    8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -14,14 +14,12 @@ void print_square(int size)
 	if (size <= 0)
 		_putchar('\n');
 
-	for (i = 0; j < (size; i++)
-			{
-			for (j = 0; j < (size); j++)
-			{
+	for (i = 0; < size; i++)
+	{
+		for (j = 0; j < (size); j++)
+		{
 			_putchar('#');
-			}
-			_putchar('\n');
-			}
-			}
-
-
+		}
+		_putchar('\n');
+	}
+}
