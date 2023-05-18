e146c0286ca26478352aa99ed75678dbae71ed83
Pontsho04 <mabopontsho>
2023-05-18 08:01:37 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -21,7 +21,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		len2++;
 
 	if (n < len2)
-		s = malloc(sizeof(char) * (len1 + n  + 1));
+		s = malloc(sizeof(char) * (len1 + n + 1));
 	else
 		s = malloc(sizeof(char) * (len1 + len2 + 1));
 
@@ -35,6 +35,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	}
 
 	while (n < len2 && i < (len1 + n))
+		s[i++] = s2[j++];
+
+	while (n >= len2 && i < (len1 + len2))
 		s[i++] = s2[j++];
 
 	s[i] = '\0';
