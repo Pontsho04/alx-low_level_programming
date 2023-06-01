490d2cda87ad072a77dbf300a26ba5596be89ac6
Pontsho04 <mabopontsho@gmail.com>
2023-06-01 09:43:32 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,7 @@
 #ifndef LISTS_H
 #define LISTS_H
+#include <stddef.h>
+
 
 /**
  * struct list_s - single linked list
@@ -13,7 +15,7 @@ typedef struct list_s
 {
 	char *str;
 	unsigned int len;
-	struct list_s *next
+	struct list_s *next;
 } list_t;
 
 size_t print_list(const list_t *h);
