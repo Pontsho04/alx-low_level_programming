b069c83891bd79315737803dc7ff70f2daf0ded6
Pontsho04 <mabopontsho@gmail.com>
2023-06-08 09:46:53 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -7,6 +7,6 @@ int set_bit(unsigned long int *n, unsigned int index);
 int get_bit(unsigned long int n, unsigned int index);
 void print_binary(unsigned long int n);
 unsigned int binary_to_uint(const char *b);
-int_putchar(char c);
+int _putchar(char c);
 
 #endif
