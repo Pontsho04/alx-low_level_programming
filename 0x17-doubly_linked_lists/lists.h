24962a3c24c97eb453497fbccfa1d58aec830634
Pontsho04 <mabopontsho@gmail.com>
2023-08-17 13:15:32 -0700

    17 doubly linked list

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,5 +1,5 @@
-#ifndef _LISTS_H
-#define _LISTS_H
+#ifndef _LISTS_
+#define _LISTS_
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -19,7 +19,6 @@ typedef struct dlistint_s
 	int n;
 	struct dlistint_s *prev;
 	struct dlistint_s *next;
-
 } dlistint_t;
 
 size_t print_dlistint(const dlistint_t *h);
